091460261623e1b837c918f7674d11a20e35c978
Arseny <irusap54@gmail.com>
2021-09-13 21:27:55 +0300

    инициалы через декоратор

--- 0809.py
+++ 0809.py
@@ -1,6 +1,23 @@
-list_=[]
-i = 0
-while i <= 50:
-    list_.append(i)
-    i+=2   
-print(list_)
+names = ['Borovoy Arseny Mikhailovich','Putin Vladimir Vladimirovich']
+
+result=[]
+
+def initials(name):
+        l = name.split()
+        return l[0] + ' ' + l[1][0:1] + '. ' + l[2][0:1] + '.'
+
+def initials_more(names):
+    return [initials(name) for name in names]
+
+def decorator(func):
+    def inner(*args, **kwargs):
+        print('Running function: ', func.__name__)
+        print('Positional arguments are: ', args)
+        print('Keyword arguments are: ', kwargs)
+        result = func(*args, **kwargs)
+        print('Result: ', result)
+        return result
+    return inner
+
+b=decorator(initials_more)
+print(b(names))
