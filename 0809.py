6e79ab2055657ca57d57c13210fe77ee52373e70
Arseny <irusap54@gmail.com>
2021-09-25 15:53:00 +0300

    четные значение через while

--- 0809.py
+++ 0809.py
@@ -1,23 +1,4 @@
-names = ['Borovoy Arseny Mikhailovich','Putin Vladimir Vladimirovich']
-
-result=[]
-
-def initials(name):
-        l = name.split()
-        return l[0] + ' ' + l[1][0:1] + '. ' + l[2][0:1] + '.'
-
-def initials_more(names):
-    return [initials(name) for name in names]
-
-def decorator(func):
-    def inner(*args, **kwargs):
-        print('Running function: ', func.__name__)
-        print('Positional arguments are: ', args)
-        print('Keyword arguments are: ', kwargs)
-        result = func(*args, **kwargs)
-        print('Result: ', result)
-        return result
-    return inner
-
-b=decorator(initials_more)
-print(b(names))
+x = 0
+while x <= 50:
+    print(x)
+    x += 2
