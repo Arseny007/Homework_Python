e08fa4a50f92016f51db47c914fa7328dc49209a
Arseny007 <irusap54@gmail.com>
2022-06-03 15:25:33 +0300

    add script

--- 11.04/11.04.22.1.passhash.sh
+++ 11.04/11.04.22.1.passhash.sh
@@ -1,7 +1,9 @@
-count=1
 old=$(cat sha_log_info.txt)
 file="log_info.txt"
 cat $file | while read line
 do
-    
+    result=$(echo $(echo $line | cut -d " " -f 1) $(echo $line | cut -d " " -f 2 | sha256sum | cut -d " " -f 1))
+    if [[ $old != *"$result"* ]]; then
+    echo $result >> sha_log_info.txt
+    fi
 done
